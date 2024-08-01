10cfcce8dc68c2f41fa111e8ad0b933317310563
jenkins <jenkins@regula.by>
2024-08-01 16:33:13 +0300

    Release 6.9.7950

--- Package.swift
+++ Package.swift
@@ -10,6 +10,6 @@ let package = Package(
             targets: ["MRZ"]),
     ],
     targets: [
-        .binaryTarget(name: "MRZ", url: "https://pods.regulaforensics.com/MRZ/6.8.7686/DocumentReaderCore_mrz_6.8.7686.zip", checksum: "0230464fcdc032809d7209f4619c25c820729bbaae74e188ee7db4c52bc7899f"),
+        .binaryTarget(name: "MRZ", url: "https://pods.regulaforensics.com/MRZ/6.9.7950/DocumentReaderCore_mrz_6.9.7950.zip", checksum: "e8b3c5887723b1b163df6869b95e5d85fc61a6cef325da77698f0891fb04fdca"),
     ]
 )
